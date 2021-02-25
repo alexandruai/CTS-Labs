3888320c7e79e689b63a02cadc2be20005ae6e2f
alexandruai <andra2699@yahoo.com>
2021-02-25 14:10:40 -0500

    What should I change in order to get a clean code

---
+++ Alexandru_Andra_Grupa1052Tema1_Java/src/com/company/Descarcare.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public interface Descarcare {
+
+    // metodă ce va returna numărul de containere rămas de descărcat de pe nava PortContainer,
+    // din tipul de container manipulat de către instanța de  Macara primită ca parametru de
+    // intrare în metodă
+    public int DescarcaContainer(PortContainer vas, Macara mc);
+
+}
